b91d974f7025508bd456dcbaf9ccdfb2f30fe523
duyi <1079240024@qq.com>
2019-10-25 11:47:57 +0800

    java程序计算完成

--- calculation/src/main/java/com/example/calculation/MyClass.java
+++ calculation/src/main/java/com/example/calculation/MyClass.java
@@ -2,6 +2,18 @@ package com.example.calculation;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("哈哈哈");
+        System.out.println(getNum(-1));
+    }
+
+    public static int getNum(int x) {
+        int a = x % 2000;
+        if (a < 0){
+            a += 2000;
+        }
+        if (a <= 30) {
+            return a;
+        } else {
+            return a - 2000;
+        }
     }
 }
