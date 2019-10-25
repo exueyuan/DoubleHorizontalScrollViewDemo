bfc4423cbcbd52df78ac3a064a5e8bdfa004b96b
duyi <1079240024@qq.com>
2019-10-25 12:14:34 +0800

    设置无限循环成功

---
+++ app/src/main/java/com/duyi/horizontalscrollviewdemo/Test.java
@@ -0,0 +1,8 @@
+package com.duyi.horizontalscrollviewdemo;
+
+public class Test {
+
+    public void main() {
+        int[] a= new int[3];
+    }
+}
--- app/src/main/java/com/duyi/horizontalscrollviewdemo/two/AutoScrollView.kt
+++ app/src/main/java/com/duyi/horizontalscrollviewdemo/two/AutoScrollView.kt
@@ -1,5 +1,6 @@
 package com.duyi.horizontalscrollviewdemo.two
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
@@ -23,19 +24,69 @@ class AutoScrollView : FrameLayout {
 
 
     private fun init() {
-
+        setOnClickListener {
+            stopAnimation()
+        }
     }
 
+    var allWidth = 0
+
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-//        super.onLayout(changed, left, top, right, bottom)
+        //所有元素的长度
+        allWidth = 0
+        for (i in 0 until childCount) {
+            allWidth += getChildAt(i).measuredWidth
+        }
+        doOnLayoutLayout()
+
+        startAnimation()
+    }
+
+    var childOffsetDistence = 0
+    private fun doOnLayoutLayout() {
+        var showLeft = 0
         for (i in 0 until childCount) {
             val child = getChildAt(i)
-            val width = child.measuredWidth
-            val height = child.measuredHeight
-            child.layout(left, top, left + width, top + height)
-            child.setOnClickListener {
-                child.layout(right-width, bottom-height, right, bottom)
-            }
+            val childWidth = child.measuredWidth
+            val childHeight = child.measuredHeight
+            val offsetShowLeft = childOffsetDistence + showLeft
+            val resultShowLeft = getNum(offsetShowLeft, allWidth, width)
+            child.layout(resultShowLeft, 0, resultShowLeft + childWidth, childHeight)
+            showLeft += childWidth
+        }
+    }
+
+
+    //定义动画
+    var animator: ValueAnimator? = null
+
+    private fun startAnimation() {
+        animator = ValueAnimator.ofInt(0, 100)
+        animator?.duration = 1000
+        animator?.repeatCount = ValueAnimator.INFINITE
+        animator?.addUpdateListener {
+            childOffsetDistence -= 5
+            doOnLayoutLayout()
+        }
+        animator?.start()
+    }
+
+    private fun stopAnimation() {
+        animator?.cancel()
+    }
+
+    private fun getNum(x: Int, allWidth: Int, screenWidth: Int): Int {
+        if (allWidth == 0) {
+            return 0
+        }
+        var a = x % allWidth
+        if (a < 0) {
+            a += allWidth
+        }
+        return if (a <= screenWidth) {
+            a
+        } else {
+            a - allWidth
         }
     }
 }
