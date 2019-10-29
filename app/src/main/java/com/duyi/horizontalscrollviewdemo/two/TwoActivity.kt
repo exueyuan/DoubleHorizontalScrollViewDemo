89f2cc7df87a6e9fc772d1500632d649e7ed6c4e
duyi <1079240024@qq.com>
2019-10-29 15:12:05 +0800

    测试子类的点击事件

--- app/src/main/java/com/duyi/horizontalscrollviewdemo/two/TwoActivity.kt
+++ app/src/main/java/com/duyi/horizontalscrollviewdemo/two/TwoActivity.kt
@@ -1,13 +1,18 @@
 package com.duyi.horizontalscrollviewdemo.two
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.duyi.horizontalscrollviewdemo.R
+import kotlinx.android.synthetic.main.activity_two.*
 
 class TwoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_two)
+        tv_222.setOnClickListener {
+            Toast.makeText(this,"222",Toast.LENGTH_SHORT).show()
+        }
     }
 }
