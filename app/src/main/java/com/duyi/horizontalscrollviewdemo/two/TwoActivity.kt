e45634cec0842a589f8c74cd9cd52904847cffee
duyi <1079240024@qq.com>
2019-11-12 15:51:15 +0800

    控件可以增加删除控件

--- app/src/main/java/com/duyi/horizontalscrollviewdemo/two/TwoActivity.kt
+++ app/src/main/java/com/duyi/horizontalscrollviewdemo/two/TwoActivity.kt
@@ -1,6 +1,7 @@
 package com.duyi.horizontalscrollviewdemo.two
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.duyi.horizontalscrollviewdemo.R
@@ -16,6 +17,13 @@ class TwoActivity : AppCompatActivity() {
         }
         fl_111.setOnClickListener {
             Toast.makeText(this,"111",Toast.LENGTH_SHORT).show()
+        }
+
+        bt_add.setOnClickListener {
+            asv_root.addView(View.inflate(this, R.layout.view_item, null))
+        }
+        bt_clear.setOnClickListener {
+            asv_root.removeAllViews()
         }
     }
 }
